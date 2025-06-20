bab94533abc0f8e06bbf4ee4f59ae06db524c943
Shubham Kumar <shubhamcse12@gmail.com>
2025-06-20 20:28:00 +0530

    initial commit

---
+++ SMS/src/App.jsx
@@ -0,0 +1,12 @@
+import LoginPage from '../components/login/LoginPage';
+import Header from '../components/header/Header';
+function App() {
+  return (
+    <div>
+      <Header />
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
